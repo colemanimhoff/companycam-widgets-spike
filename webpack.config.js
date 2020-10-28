9e0ddf4709a15a18c8f502d54984fde6c63f23d5
Coleman Imhoff <colemanimhoff@gmail.com>
2020-10-28 14:28:34 -0600

    reverting to latest version of webpack 4; fixed typos in config file

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,8 @@ const path = require("path");
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path: path.join(__dirname, "build"),
-    publicPath: "/",
-    filename: "index.js",
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
@@ -26,11 +25,6 @@ module.exports = {
         ],
       },
     ],
-  },
-  output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
-    filename: "bundle.js",
   },
   plugins: [
     new HtmlWebPackPlugin({
