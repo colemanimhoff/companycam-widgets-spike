9cb6ddbf166b7413a610ab9b8e2eab39864d5426
Coleman Imhoff <colemanimhoff@gmail.com>
2020-10-30 13:40:31 -0600

    testing out css selectors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ const App = () => (
   <>
     <div>
       <p className="red">I am red</p>
-      <p className="blue">I am blue</p>
+      <p id="blue">I am blue</p>
       <p className="green">I am green</p>
     </div>
   </>
